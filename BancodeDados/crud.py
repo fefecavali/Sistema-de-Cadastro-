b9a6b9e7933337554eddd902d740899d962a4061
Fernanda Cavali <fefecavali@gmail.com>
2023-11-29 00:09:57 -0300

    updateBD_FER

--- BancodeDados/crud.py
+++ BancodeDados/crud.py
@@ -1,30 +1,47 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
-import psycopg2
+from sqlalchemy.orm import sessionmaker
 
+#Configurações
 engine = create_engine('postgresql://postgres:pgadmin123@localhost:5432/mecanica_teste')
 
 Session = sessionmaker(bind=engine)
 session = Session()
 base = declarative_base()
 
-class Pecas(base):
-    __tablename__ = 'pecas'
+#Entidades
+class Customer(base):
+    __tablename__ = 'customer'
 
-    nome = Column(String, primary_key=True)
-    preco = Column(Integer, nullable=False)
-
+    id_customer = Column(String, primary_key=True)
+    number_customer = Column(Integer, nullable=False)
+    address_customer = Column(String, nullable=False)
+    name_customer = Column(String, nullable=False)
+    car = Column(String, nullable=False)
+    
     def __repr__ (self):
-        return f'oi'
+        return f" Cliente {self.name_customer}, proprietário {self.car}
+                Cpf : {self.id_customer} 
+                Tel : {self.number_customer}"
+    
+#SQL
 
-peca1 = Pecas(nome='Valmir', preco=100)
-
-session.add(peca1)
+## ----> INSERT <----
+data_insert = Customer ( id_customer="21499845678", number_customer=998058909 ,address_customer="Rua Mambore bonita" ,name_customer="Fernanda", car="Ferrari 2025" )
 session.commit()
 
-# consulta = session.query(Pecas).all()
-# print(consulta[0])
+## ----> SELECT <----
+data = session.query(Customer).all()
+session.commit()
 
-session.close()
+## ----> UPDATE <----
+session.query(Customer).filter(Customer.id_customer == "21499845678").update( { "name_customer" : "Fernanda Cavali"})
+session.commit()
+
+## ----> DELETE <----
+# session.query(Customer).filter(Customer.id_customer ==="21499845678").delete()
+# session.commit()
+
+
+session.close()           #Fechar conexão
+
