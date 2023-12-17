1caeed27b7cf30c32920cca4b847a0808dcbb408
Sara linda <130059301+saraqwe123@users.noreply.github.com>
2023-12-17 09:32:35 -0300

    Delete BancodeDados/trabalho directory

--- BancodeDados/trabalho/static/js/main.js
+++
@@ -1,7 +0,0 @@
-let trilho = document.getElementById('trilho')
-let body = document.getElementById('body')
-
-trilho.addEventListener('click', ()=> {
-    trilho.classList.toggle('dark')
-    body.classList.toggle('dark')
-})
